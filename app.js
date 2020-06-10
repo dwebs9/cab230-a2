c65913849d95fd927498e355d73951580480ad14
CAB230 <cab230@VDI-VL24B-291.qut.edu.au>
2020-06-10 23:28:44 +1000

    Working first route

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-const db = require('./database/db');
+const options = require('./knexfile.js');
+const knex = require('knex')(options);
+
+const swaggerUI =  require('swagger-ui-express'); 
+yaml = require('yamljs');
+swaggerDocument = yaml.load('./docs/swagger.yaml');  
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -20,10 +25,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(db);
+
+
+app.use((req, res, next)=>{
+  req.db = knex
+  next()
+})
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,74 @@
 const express = require('express');
 const mysql = require('mysql');
 const router = express.Router();
+const options = require('../knexfile');
+const knex = require('knex')(options);
 
+// Knex test
+// router.get('/knex', function(req,res,next) {
+//   req.db.raw(   "SELECT VERSION()").then(
+//     (version) => console.log (( version[0][ 0]))
+//     ).catch((err) => {  console.log( err );  throw err }) 
+//     res.send("Version Logged successfully");  
+//     }); 
 
-
-
-
+// http://131.181.190.87:3005/stocks/symbols?industry=Health
 // Stocks/Symbols Route
 router.get("/stocks/symbols", function(req,res){
-    var query = "SELECT DISTINCT name, symbol, industry FROM ??";
-    var table = ["stocks"];  
-    query = mysql.format(query,table);
-      req.db.query(query,function(err,rows){
+  builder = req.db;
+  console.log("builder's value is:");
+  console.log(req.query.industry);
+
+   builder.from('stocks').distinct("name", "symbol", "industry")
+    .modify(function (builder){
+        if(req.query.industry){
+          console.log("industry queried");
+          console.log(req.query.industry);
+          builder.where('industry','like', req.query.industry);
+
+        }
+      }).then(function (rows) {
+        console.log(rows);
+        res.json(rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({"Error" : true, "Message" : "Error executing MySQL query"})
+    })
+    
+
+    
+    //  req.db.distinct().from('stocks').select("name", "symbol", "industry")
+    // .then((rows)=>{
+    //   res.json({"Error" : false, "Message" : "Success", "stocks" : rows})
+    // })
+
+  
+ 
+});
+
+
+router.get("/stocks/:id", function(req,res){
+  var query = "SELECT DISTINCT name, symbol, industry FROM ?? where ??=?"; 
+  // console.log("req.params.id value is:")
+  // console.log(req.params.id)
+  var table =['stocks','symbol',req.params.id];
+  query = mysql.format(query, table);
+    req.db.query(query,function(err,rows){
   if(err) {
     res.json({"Error" : true, "Message" : "Error executing MySQL query"});
   } else {
   res.json({"Error" : false, "Message" : "Success", "stocks" : rows});
    console.log(rows);
   }
-  });
-  console.log("serving Route: /stocks/symbols")
- 
-})
-// /stocks/{symbol}
-router.get("/stocks/:id", function(req,res){
-  console.log("serving Route: /stocks/:id")
-})
+   });
+
+});
 
 // /stocks/authed/{symbol}
 router.get("/stocks/authed/{symbol}", function(req,res){
   console.log("serving Route: /stocks/authed/{symbol}")
-})
+});
 
-
-// /* GET home page. */
-// router.get("/api/city",function(req,res){
-//   var query = "SELECT name, district FROM ??";
-//   var table = ["city"];
-//   query = mysql.format(query,table);
-//   req.db.query(query,function(err,rows){
-//   if(err) {
-//     res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-//   } else {
-//   res.json({"Error" : false, "Message" : "Success", "City" : rows});
-//   }
-//   });
-// });
-
-// router.get("/api/city/:CountryCode",function(req,res){
-//   var query = "SELECT * FROM ?? WHERE ??=?";
-//   var table = ["city","CountryCode",req.params.CountryCode];
-//   query = mysql.format(query,table);
-//   req.db.query(query,function(err,rows){
-//     if(err) {
-//     res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-//     } else {
-//     res.json({"Error" : false, "Message" : "Success", "Cities" : rows});
-//     }
-//     });
-//    });
 
 module.exports = router;
